add490991f3537c45f4be6de28ef0fe08c6bf91d
GijunMoon (기준 문) <gijunmoon@moonstudio.kro.kr>
2019-12-13 22:21:08 +0900

    Add files via upload

---
+++ Crafting/CraftingRecipes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Recipe", menuName = "CraftingRecipe", order = 2)]
+public class CraftingRecipes : ScriptableObject
+{
+    [SerializeField]
+    string ItemName;
+
+    [SerializeField]
+    string description;
+
+    [SerializeField]
+    string [] recipes = new string[6];
+
+
+    public string sName => ItemName;
+
+    public string sDesc => description;
+
+    public string [] nRes => recipes;
+}
+
---
+++ Crafting/CraftingSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingSystem : MonoBehaviour
+{
+    public Text result;
+    public Text resultdec;
+    public string[] items = new string [6];
+    public CraftingRecipes recipes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void OnClickCraft()
+    {
+        int i = 0;
+        while (i > -1)
+        {
+            i++;
+
+            if (items[i - 1] == recipes.nRes[i - 1])
+            {
+                if (i == recipes.nRes.Length - 1)
+                {
+                    result.text = recipes.sName;
+                    resultdec.text = recipes.sDesc;
+                    break;
+                }
+            }
+            else
+            {
+                result.text = "Nothing";
+                resultdec.text = "Sorry..";
+                break;
+            }
+        }
+    }
+}
